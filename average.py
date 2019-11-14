a716a70fb569e8f68adceca46f15c787f4a3f58b
BaksiLi <myself@baksili.codes>
2019-11-14 12:27:10 +0000

    Add average function.

--- average.py
+++ average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Brusli and Baksi
+
+
+def average(list_of_list):
+    return [sum(x) / len(x) for x in list_of_list]
+
+
+# print(average([[1,2],[3,1]]))
