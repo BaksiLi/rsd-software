fbb2bd53676d45723da76bd222772a24eee4b5c5
BaksiLi <myself@baksili.codes>
2019-11-14 12:38:10 +0000

    Add csv_write function to rwcsv

--- rwcsv.py
+++ rwcsv.py
@@ -3,5 +3,8 @@ import csv
 def csv_read(csvfile):
     csv.reader(csvfile, delimiter=',', quotechar='|')
 
-def csv_write():
+def csv_write(file_name, result):
+    with open("output.csv", "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(result)
     
